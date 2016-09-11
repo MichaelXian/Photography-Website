f68a4129c56b6a22805073792aa1335ad2a0de7e
nightfury2986 <michaelxian1@live.com>
2016-09-11 15:38:05 -0700

    Add files via upload

---
+++ gallery/architect.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title | Gallery</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href='css/gallery.css'>
+    </head>
+    
+    <body id="body" onload="setup()" class="greenBg">
+       
+       <!--    <button id="debug" onclick="tempDebug()">Magical Debugging Button</button> -->
+        
+        <div id="menu-bar"> 
+            <a href="index.html" class="menu-item">
+                <div id="home">
+                    <p>Home</p>
+                </div> <!-- #home -->
+            </a>
+             <a href="gallery.html" class="menu-item selected">
+                 <div id="gallery">
+                    <p>Gallery</p>
+                </div><!--#Gallery -->
+            </a>
+              <a href="pricing.html" class="menu-item">
+                <div id="about">
+                    <p>Pricing</p>
+                </div><!-- #about -->
+            </a>
+            <a href="booking.html"class="menu-item">
+                <div id="contact">
+                    <p>Booking</p><!-- #contact-->
+                </div>
+            </a>
+        </div><!-- #menu-bar-->
+        
+        <h1>Architectural</h1>
+        
+         <script>
+   
+        </script>
+    </body>
+</html>
